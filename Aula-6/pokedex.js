a403c2093c34b45030d2f37e6d42c7e9d672b9a0
vitorreisler <vitorreisler@gmail.com>
2023-02-18 11:20:36 +0200

    Pokedex

---
+++ Aula-6/pokedex.js
@@ -0,0 +1,16 @@
+let pokedex = []
+
+function insertPokemon() {
+    let pokeName = pokedex.pokeName
+    let pokeType = pokedex.pokeType
+    pokedex.push ({
+        pokeName: prompt('Which pokemon do you want to insert ? '),
+        pokeType: prompt (`Which kind of pokemon ${pokeName} is ?`)
+    })
+    
+}
+
+function toShow() {
+    console.log (pokedex)
+
+}
